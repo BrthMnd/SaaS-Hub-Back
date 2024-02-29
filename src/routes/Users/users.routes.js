0ba821a083892d16e7c1fa9a41048c88ecf46ef3
xChichoTop <johanparra2319@gmail.com>
2024-02-29 14:36:39 -0500

    Details

--- src/routes/Users/users.routes.js
+++ src/routes/Users/users.routes.js
@@ -4,10 +4,13 @@ import {
   getCookie,
   getFirst,
 } from "../../controllers/users/user.controller.js";
+import { Delete } from "../../controllers/users/user.controller.js";
+
 const route = Router();
 
 route.get("/", getMany.bind());
 route.get("/cookie", getCookie.bind());
 route.get("/:id", getFirst.bind());
+route.delete("/:id", Delete);
 
 export default route;
